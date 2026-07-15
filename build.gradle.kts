4d892eed8318f1e92d3743b622d27182908d1bb3
atro2829-hub <atro2829@gmail.com>
2026-07-15 22:05:04 +0300

    fix: kotlin-kapt version compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("kotlin-kapt") version "1.9.25" apply false
+    id("kotlin-kapt") version "1.9.22" apply false
 }
